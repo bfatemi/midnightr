6459e09a6807c99b9cedcd5565827396cf231dcc
Bobby Fatemi <bfatemi@hpds.io>
2017-09-11 02:52:13 +0000

    all files

---
+++ R/mio_track.R
@@ -0,0 +1,19 @@
+#' Ping Location
+#'
+#' @param ... location parameters
+#'
+#' @import data.table
+#' @importFrom pryr named_dots
+#'
+#' @export
+mio_track <- function(...){
+  param <- data.table::as.data.table(pryr::named_dots(...))
+  return("SUCCESS IS RELATIVE")
+
+  con <- mongo(collection = "location_pings",
+               db = "MIO",
+               url = "mongodb://readwrite:MIO@138.197.195.106:51138/",
+               verbose = TRUE)
+}
+
+
